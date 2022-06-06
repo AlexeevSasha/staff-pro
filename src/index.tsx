ba27f6920f3b71e2a7adccc45103132d008b9271
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:29:57 +0300

    delete strickmode

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App';
-import 'antd/dist/antd.min.css';
-import './assets/styles/index.css';
 import {ThemeProvider} from "styled-components";
 import {theme} from "./assets/styles/theme";
 import {Provider} from "react-redux";
 import {store} from './core/redux/store'
 
+import 'antd/dist/antd.min.css';
+import './assets/styles/index.css';
 
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
             <ThemeProvider theme={theme}>
@@ -23,6 +22,5 @@ root.render(
             </ThemeProvider>
             </Provider>
         </BrowserRouter>
-    </React.StrictMode>
 );
 

6b6fa7a497d795e1182eea5970ce0ee95c5cbe94
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:33:11 +0300

    customers dto

---
+++ src/api/customers/customersDto.ts
@@ -0,0 +1,21 @@
+export interface ICustomers {
+    email: string;
+    name: string;
+    lastname: string;
+    patronymic: string;
+    day: number;
+    month: string;
+    year: number;
+    phone?: string;
+    sex: 'man' | 'woman';
+}
+
+
+export type EmployeeType = ICustomers & { position: string };
+export type SeekerType = ICustomers;
+
+export type EmployeeFromServiceType = EmployeeType & {id: number};
+export type SeekerFromServiceType = SeekerType & {id: number};
+
+
+

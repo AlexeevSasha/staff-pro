c5b1a86e3cdb904fd82043406ac3aa4d9c7c7dcb
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:30:42 +0300

    add link menu

--- src/common/components/MainLayout/Menu.tsx
+++ src/common/components/MainLayout/Menu.tsx
@@ -1,7 +1,5 @@
-
-
 import React, {FC} from 'react';
-import { Menu} from 'antd';
+import {Menu} from 'antd';
 import {
     PieChartOutlined,
     RiseOutlined,
@@ -10,8 +8,9 @@ import {
     UserOutlined,
     InfoCircleOutlined,
 } from '@ant-design/icons';
-import type { MenuProps } from 'antd/es/menu';
+import type {MenuProps} from 'antd/es/menu';
 import styled from "styled-components";
+import {NavLink} from "react-router-dom";
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -30,37 +29,31 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Dashboard', '1', <PieChartOutlined />),
-    getItem('Reports', '2', <RiseOutlined />),
-    getItem('Documents', 'sub1', <FileDoneOutlined />, [
-        getItem('Invoices', '3'),
-        getItem('Drafts', '4'),
-        getItem('Templates', '5'),
+    getItem(<NavLink to='/dashboard'>Dashboard</NavLink>, '1', <PieChartOutlined/>),
+    getItem(<NavLink to='/reports'>Reports</NavLink>, '2', <RiseOutlined/>),
+    getItem('Documents', 'sub1', <FileDoneOutlined/>, [
+        getItem(<NavLink to='/documents/invoices'>Invoices</NavLink>, '3'),
+        getItem(<NavLink to='/documents/drafts'>Drafts</NavLink>, '4'),
+        getItem(<NavLink to='/documents/templates'>Templates</NavLink>, '5'),
     ]),
-    getItem('Customers', 'sub2',<UserOutlined />, [
-        getItem('Option 7', '6'),
-        getItem('Option 8', '7'),
-    ]),
-    getItem('Settings', '8', <SettingOutlined />),
-    getItem('Help & Contact', '9', <InfoCircleOutlined />),
+    getItem(<NavLink to='/customers'>Customers</NavLink>, '6', <UserOutlined/>),
+    getItem('Settings', '7', <SettingOutlined/>),
+    getItem('Help & Contact', '8', <InfoCircleOutlined/>),
 ];
 
 export const LeftMenu: FC = () => {
 
     return (
-            <MenuStyle
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                mode='inline'
-                items={items}
-            />
+        <MenuStyle
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode='inline'
+            items={items}
+        />
     );
 }
 
 const MenuStyle = styled(Menu)`
-  position: fixed;
-  left: 0;
-  top: 0;
   padding-top: 50px;
   height: 100vh;
   width: 200px;

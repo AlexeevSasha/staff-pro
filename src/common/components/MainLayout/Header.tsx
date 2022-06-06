1f2eddf2f4635e895798a9dcbad339dcd60ecaed
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:46:45 +0300

    add logout

--- src/common/components/MainLayout/Header.tsx
+++ src/common/components/MainLayout/Header.tsx
@@ -1,27 +1,30 @@
 import {Layout, Button, Typography} from 'antd';
 import styled from 'styled-components';
-import {useAppSelector} from "../../../core/redux/reduxType";
+import {useAppDispatch, useAppSelector} from "../../../core/redux/reduxType";
 import {BreadcrumbComponent} from "./BreadcrumbComponent";
 import {useLocation} from "react-router-dom";
+import {logout} from "../../../modules/auth/authSlice";
+import {useCallback} from "react";
 
 const {Header} = Layout;
 const {Title, Paragraph} = Typography
 
 export const HeaderMenu = () => {
+    const dispatch = useAppDispatch()
     const location = useLocation();
     const {user} = useAppSelector(state => state.auth)
-
+    const exit = useCallback(() => dispatch(logout()), [])
     const path = location.pathname.split('/')
     const lastElem = path[path.length - 1].charAt(0).toUpperCase() + path[path.length - 1].slice(1).toLowerCase();
 
     return (
         <Header style={{background: 'white', padding: '10px 20px', height: '110px'}}>
             <BreadcrumbComponent/>
-            <Flex >
+            <Flex>
                 <Title level={3} style={{margin: 0}}>{lastElem}</Title>
                 <User>
                     <Paragraph>{user?.name} {user?.lastname}</Paragraph>
-                    <Button>Выйти</Button>
+                    <Button onClick={exit}>Выйти</Button>
                 </User>
             </Flex>
         </Header>
@@ -37,6 +40,7 @@ const Flex = styled.div`
 
 const User = styled(Flex)`
   gap: 15px;
+
   & > div {
     margin: 0;
     line-height: 20px;

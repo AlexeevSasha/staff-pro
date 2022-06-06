6e29813f7a1d9ccd689e9029ef05764701cadd17
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:31:51 +0300

    add documents and customers

--- src/core/redux/store.ts
+++ src/core/redux/store.ts
@@ -1,11 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authSlice from "../../modules/auth/authSlice";
+import documentsSlice from '../../modules/documents/documentSlice'
+import customersSlice from "../../modules/customers/customersSlice";
 
 export const store = configureStore({
     reducer: {
-    auth: authSlice
+        auth: authSlice,
+        documents: documentsSlice,
+        customers: customersSlice
     }
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch;
+ export type RootState = ReturnType<typeof store.getState>
+ export type AppDispatch = typeof store.dispatch

1360a9f27410de2ff535f6407329bb0ba115e5ff
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:28:25 +0300

    add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import {LayoutAuth} from "./modules/auth/components/LayoutAuth";
 import {SignIn} from "./modules/auth/page/SignIn";
 import {SignUp} from "./modules/auth/page/SignUp";
 import {ForgotPassword} from "./modules/auth/page/ForgotPassword";
 import {AuthProvider, UserProvider} from "./common/hooks/AuthProvider";
 import {MainLayout} from "./common/components/MainLayout";
+import {Dashboard} from "./modules/dashboard/Dashboard";
+import {Reports} from "./modules/dashboard/Reports";
+import {Templates} from "./modules/documents/Templates";
+import {Drafts} from "./modules/documents/Drafts";
+import {Invoices} from "./modules/documents/Invoices";
+import {Customers} from "./modules/customers/Customers";
 
 
 const App = () => {
     return (
         <Routes>
             <Route element={<UserProvider/>}>
-            <Route path='/' element={<LayoutAuth/>}>
-                <Route index element={<SignIn/>}/>
-                <Route path='register' element={<SignUp/>}/>
-                <Route path='forgot-password' element={<ForgotPassword/>}/>
-            </Route>
+                <Route path='/' element={<LayoutAuth/>}>
+                    <Route index element={<SignIn/>}/>
+                    <Route path='register' element={<SignUp/>}/>
+                    <Route path='forgot-password' element={<ForgotPassword/>}/>
+                </Route>
             </Route>
             <Route element={<AuthProvider/>}>
-                <Route path='dashboard' element={<MainLayout/>}>
+                <Route  element={<MainLayout/>}>
+                    <Route path='/dashboard' element={<Dashboard/>}/>
+                    <Route path='/reports' element={<Reports/>}/>
+                    <Route path='/documents' >
+                        <Route index element={<Navigate to='invoices'/>}/>
+                        <Route path='invoices' element={<Invoices/>}/>
+                        <Route path='drafts' element={<Drafts/>}/>
+                        <Route path='templates' element={<Templates/>}/>
+                    </Route>
+                    <Route path='/customers' element={<Customers/>}/>
                 </Route>
             </Route>
         </Routes>

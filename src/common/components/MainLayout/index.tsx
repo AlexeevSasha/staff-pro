07fef11f1b16bf20c41f801eacad363ec96419ee
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-06 17:29:35 +0300

    add mobile menu

--- src/common/components/MainLayout/index.tsx
+++ src/common/components/MainLayout/index.tsx
@@ -1,34 +1,84 @@
 import {LeftMenu} from "./Menu";
 import styled from "styled-components";
+import {HeaderMenu} from "./Header";
+import {Outlet} from "react-router-dom";
+import {useState} from "react";
+import {Button, Layout} from 'antd';
+import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
 
+
+const {Content} = Layout;
 
 export const MainLayout = () => {
+    const [visible, setVisible] = useState(false);
+    const toggle = () => setVisible(!visible);
     return (
-        <div style={{background: '#F0F2F5', height: '100vh'}}>
-            <Header>asfasdfsdfsaddasd</Header>
-            <LeftMenu/>
-            <div style={{marginLeft: 200}}>
-                <Content>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem fugit laudantium nostrum
-                    reprehenderit
-                    temporibus. Aliquid delectus enim est eum facere fugit. Assumenda autem cum cumque doloribus esse,
-                    ipsam
-                    itaque omnis.
-                </Content>
+        <>
+            <BlackDiv visible={visible} onClick={toggle}/>
+            <div style={{background: '#F0F2F5', height: '100vh'}}>
+                <Wrapper><HeaderMenu/></Wrapper>
+                <LeftMenuStyle visible={visible}>
+                    <LeftMenu/>
+                    <Button onClick={toggle} type="primary" icon={visible ? <MenuFoldOutlined/> : <MenuUnfoldOutlined />}/>
+                </LeftMenuStyle>
+                <Wrapper>
+                    <ContentStyle>
+                        <Outlet/>
+                    </ContentStyle>
+                </Wrapper>
             </div>
-        </div>
+        </>
     );
 }
 
-const Header = styled.div`
- margin-left: 200px;
-  background: white;
+const ContentStyle = styled(Content)`
+    min-height: calc(100vh - 110px);
+     background: white;
+  padding: 0 20px;
 `
 
-const Content = styled.div`
-  background: white;
+const BlackDiv = styled.div<{ visible: boolean }>`
+  position: fixed;
+  display: none;
+  width: 100vw;
+  height: 100%;
+  opacity: .5;
+  z-index: 1;
+  background: black;
+  @media ${({theme}) => theme.media._980} {
+    display: ${({visible}) => visible ? 'block' : 'none'};
+  }
+ 
 `
 
+const Wrapper = styled.div`
+  margin-left: 200px;
+  @media ${({theme}) => theme.media._980} {
+    margin-left: 0;
+  }
+`
+const LeftMenuStyle = styled.div<{ visible: boolean }>`
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 3;
+  & > button {
+    display: none;
+    position: absolute;
+    right: -32px;
+    top: 30px;
+    opacity: .5;
+    
+  }
+}
+
+@media ${({theme}) => theme.media._980} {
+  transition: all .3s linear;
+  left: ${({visible}) => visible ? '0' : '-200px'};
+  & > button {
+    display: block;
+  }
+`
 
 
 

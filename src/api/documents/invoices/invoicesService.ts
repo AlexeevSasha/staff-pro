657692ec0ed0b78ac04de25dcd7a94ad5bdd522e
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:06:16 +0300

    refactor

--- src/api/documents/invoices/invoicesService.ts
+++ src/api/documents/invoices/invoicesService.ts
@@ -1,9 +1,6 @@
-import {post, get} from "../baseRequest";
+import {post, get} from "../../baseRequest";
 import {IInvoices, InvoicesFromServer} from "./invoicesDto";
 
 
-export const addInvoicesService =  (data: IInvoices): Promise<InvoicesFromServer[]> => {
-    return  post('invoices', JSON.stringify(data))
-}
-
+export const addInvoicesService =  (data: IInvoices): Promise<InvoicesFromServer[]> => post('invoices', JSON.stringify(data))
 export const getAllInvoicesService =  (): Promise<InvoicesFromServer[]> => get('invoices')

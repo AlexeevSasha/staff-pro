38bc69988a5cd037ca611786e2616dfb8bdf9786
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:02:28 +0300

    dashboard

--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -1,11 +1,29 @@
-import {FC} from "react";
+import {FC, useEffect} from "react";
+import {InvoicesGraphic} from "./components/InvoicesGraphic";
+import styled from "styled-components";
+import {DiagramCustomers} from "./components/DiagramCustomers";
+import {useAppDispatch} from "../../core/redux/reduxType";
+import {getAllInvoicesThunk} from "../documents/documentThunk";
+import {getAllEmployeeThunk, getAllSeekerThunk} from "../customers/customersThunk";
 
 
 
 export const Dashboard: FC = () => {
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        dispatch(getAllInvoicesThunk())
+        dispatch(getAllEmployeeThunk())
+        dispatch(getAllSeekerThunk())
+    }, [dispatch])
+
     return (
-        <div>
-            Dashboard
-        </div>
+        <Wrapper>
+           <DiagramCustomers/>
+           <InvoicesGraphic/>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+  padding: 20px;
+`

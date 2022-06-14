441e5acc105432b90d03a88b344404ba440d16a6
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:08:39 +0300

    add adaptive

--- src/modules/documents/components/TableInvoices.tsx
+++ src/modules/documents/components/TableInvoices.tsx
@@ -1,15 +1,28 @@
 import {FC} from "react";
-import { Table } from 'antd';
-import {columns} from "../hooks/invoicesTable";
-import { InvoicesFromServer} from "../../api/invoices/invoicesDto";
+import {Spin, Table} from 'antd';
+import {columns} from "../../../common/hooks/invoicesTable";
+import {InvoicesFromServer} from "../../../api/documents/invoices/invoicesDto";
+import {useAppSelector} from "../../../core/redux/reduxType";
+import {selectLoadingDocuments} from "../documentSlice";
+import { WrapperDisplayNone768} from "../../../common/components/Style";
+
 
 
 interface IProps {
-    invoices : InvoicesFromServer[]
+    invoices: InvoicesFromServer[]
 }
 
-export const TableInvoices:FC<IProps> = ({invoices}) => {
+export const TableInvoices: FC<IProps> = ({invoices}) => {
+    const loading = useAppSelector(selectLoadingDocuments)
     return (
-        <Table columns={columns} dataSource={invoices}   rowKey="id"    pagination={{pageSize: 10}}/>
+        <WrapperDisplayNone768>
+            <Spin tip="Loading..." spinning={loading}>
+                <Table columns={columns} dataSource={invoices} rowKey="id" pagination={{pageSize: 10}}
+                       scroll={{x: 900}}/>
+            </Spin>
+        </WrapperDisplayNone768>
     )
 }
+
+
+

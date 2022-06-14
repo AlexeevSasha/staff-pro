688d60a613e83d992939bd13ae12fc83fb1a0f5a
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:03:19 +0300

    add draft thunk

--- src/modules/documents/documentThunk.ts
+++ src/modules/documents/documentThunk.ts
@@ -1,8 +1,14 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import {IInvoices, InvoicesFromServer} from "../../api/invoices/invoicesDto";
-import {addInvoicesService, getAllInvoicesService} from "../../api/invoices/invoicesService";
+import {IInvoices, InvoicesFromServer} from "../../api/documents/invoices/invoicesDto";
+import {addInvoicesService, getAllInvoicesService} from "../../api/documents/invoices/invoicesService";
+import {DraftForm, IDrafts} from "../../api/documents/drafts/draftsDto";
+import {
+    deleteByIDDraftService,
+    getAllDraftsService,
+    getChangeByIDDraftService
+} from "../../api/documents/drafts/draftsService";
 
-
+//invoices
 export const addInvoicesThunk = createAsyncThunk<InvoicesFromServer[], IInvoices>(
     'documents/addInvoices',
     async (data ) => {
@@ -15,7 +21,32 @@ export const getAllInvoicesThunk = createAsyncThunk<InvoicesFromServer[]>(
     'documents/getAllInvoices',
     async () => {
         const response = await getAllInvoicesService()
-        localStorage.setItem('test', JSON.stringify(response[0]))
         return response
     }
 )
+
+
+//drafts
+export const getAllDraftsThunk = createAsyncThunk<IDrafts[]>(
+    'documents/getAllDrafts',
+    async () => {
+        const response = await getAllDraftsService()
+        return response
+    }
+)
+
+export const changeDraftThunk = createAsyncThunk<IDrafts, {id: number, data: DraftForm}>(
+    'documents/changeDraft',
+    async ({id, data}) => {
+        const response = await getChangeByIDDraftService(id, data)
+        return response
+    }
+)
+
+export const deleteDraftThunk = createAsyncThunk<void, number>(
+    'documents/deleteDraft',
+    async (id) => {
+        await deleteByIDDraftService(id)
+    }
+)
+

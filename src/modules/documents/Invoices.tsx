90352bba1ddc9b2bf4772686688751ff448191c9
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:06:02 +0300

    add adaptive cards

--- src/modules/documents/Invoices.tsx
+++ src/modules/documents/Invoices.tsx
@@ -1,45 +1,52 @@
 import {FC, useCallback, useEffect, useState} from "react";
 import {Tabs, Button} from 'antd';
 import styled from "styled-components";
-import {TableInvoices} from "../../common/components/TableInvoices";
-import {AddNewInvoices} from "../../common/components/AddNewInvoices";
+import {TableInvoices} from "./components/TableInvoices";
+import {AddNewInvoices} from "./components/AddNewInvoices";
 import {useAppDispatch, useAppSelector} from "../../core/redux/reduxType";
 import {getAllInvoicesThunk} from "./documentThunk";
+import {selectInvoices} from "./documentSlice";
+import {CardsInvoices} from "./components/CardsInvoices";
 
 const {TabPane} = Tabs;
 
 
 export const Invoices: FC = () => {
     const dispatch = useAppDispatch()
-    const {invoices} = useAppSelector(state => state.documents)
+    const invoices = useAppSelector(selectInvoices)
     const [visible, setVisible] = useState(false);
     const showDrawer = useCallback(() => setVisible(true), [])
     const onClose = useCallback(() => setVisible(false), [])
 
     useEffect(() => {
         dispatch(getAllInvoicesThunk())
-    }, [])
+    }, [dispatch])
 
     if (!invoices) return null;
     return (
         <>
             <TabsStyle defaultActiveKey="1"
                        tabBarExtraContent={<Button onClick={showDrawer} type='primary'>+ Add new invoice</Button>}>
-                <TabPaneStyle tab="All invoices" key="1">
-                    <TableInvoices invoices={invoices}/>
-                </TabPaneStyle>
-                <TabPaneStyle tab="Due" key="2">
-                    <TableInvoices invoices={invoices.filter(el => el.type === 'due')}/>
-                </TabPaneStyle>
-                <TabPaneStyle tab="Paid" key="3">
-                    <TableInvoices invoices={invoices.filter(el => el.type === 'paid')}/>
-                </TabPaneStyle>
-                <TabPaneStyle tab="Unpaid" key="4">
-                    <TableInvoices invoices={invoices.filter(el => el.type === 'unpaid')}/>
-                </TabPaneStyle>
-                <TabPaneStyle tab="Archived" key="5">
-                    <TableInvoices invoices={invoices.filter(el => el.type === 'archived')}/>
-                </TabPaneStyle>
+                    <TabPaneStyle tab="All invoices" key="1">
+                        <TableInvoices invoices={invoices}/>
+                       <CardsInvoices invoices={invoices}/>
+                    </TabPaneStyle>
+                    <TabPaneStyle tab="Due" key="2">
+                        <TableInvoices invoices={invoices.filter(el => el.type === 'due')}/>
+                        <CardsInvoices invoices={invoices.filter(el => el.type === 'due')}/>
+                    </TabPaneStyle>
+                    <TabPaneStyle tab="Paid" key="3">
+                        <TableInvoices invoices={invoices.filter(el => el.type === 'paid')}/>
+                        <CardsInvoices invoices={invoices.filter(el => el.type === 'paid')}/>
+                    </TabPaneStyle>
+                    <TabPaneStyle tab="Unpaid" key="4">
+                        <TableInvoices invoices={invoices.filter(el => el.type === 'unpaid')}/>
+                        <CardsInvoices invoices={invoices.filter(el => el.type === 'unpaid')}/>
+                    </TabPaneStyle>
+                    <TabPaneStyle tab="Archived" key="5">
+                        <TableInvoices invoices={invoices.filter(el => el.type === 'archived')}/>
+                        <CardsInvoices invoices={invoices.filter(el => el.type === 'archived')}/>
+                    </TabPaneStyle>
             </TabsStyle>
             <AddNewInvoices closeDrawer={onClose} visible={visible}/>
         </>
@@ -48,6 +55,15 @@ export const Invoices: FC = () => {
 
 const TabsStyle = styled(Tabs)`
   gap: 20px;
+
+  &.ant-tabs>.ant-tabs-nav, .ant-tabs>div>.ant-tabs-nav{
+    padding: 0 20px;
+    background: white;
+    margin: 0;
+  }
+  &.ant-tabs .ant-tabs-content-holder {
+    margin: 0 20px;
+  }
 `
 
 const TabPaneStyle = styled(TabPane)`

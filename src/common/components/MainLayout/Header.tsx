ce6df12a4319832137a155064e7ae02d19a1b1af
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:04:21 +0300

    header

--- src/common/components/MainLayout/Header.tsx
+++ src/common/components/MainLayout/Header.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import {useAppDispatch, useAppSelector} from "../../../core/redux/reduxType";
 import {BreadcrumbComponent} from "./BreadcrumbComponent";
 import {useLocation} from "react-router-dom";
-import {logout} from "../../../modules/auth/authSlice";
+import {logout, selectUser} from "../../../modules/auth/authSlice";
 import {useCallback} from "react";
 
 const {Header} = Layout;
@@ -12,7 +12,7 @@ const {Title, Paragraph} = Typography
 export const HeaderMenu = () => {
     const dispatch = useAppDispatch()
     const location = useLocation();
-    const {user} = useAppSelector(state => state.auth)
+    const user = useAppSelector(selectUser)
     const exit = useCallback(() => dispatch(logout()), [])
     const path = location.pathname.split('/')
     const lastElem = path[path.length - 1].charAt(0).toUpperCase() + path[path.length - 1].slice(1).toLowerCase();
@@ -20,7 +20,7 @@ export const HeaderMenu = () => {
     return (
         <Header style={{background: 'white', padding: '10px 20px', height: '110px'}}>
             <BreadcrumbComponent/>
-            <Flex>
+            <Flex style={{marginTop: 10}}>
                 <Title level={3} style={{margin: 0}}>{lastElem}</Title>
                 <User>
                     <Paragraph>{user?.name} {user?.lastname}</Paragraph>
@@ -35,7 +35,12 @@ const Flex = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-top: 10px;
+  
+  @media ${({theme}) => theme.media._480} {
+    & > h3 {
+      font-size: 16px;
+    }
+  }
 `
 
 const User = styled(Flex)`
@@ -44,5 +49,16 @@ const User = styled(Flex)`
   & > div {
     margin: 0;
     line-height: 20px;
+  }
+
+  @media ${({theme}) => theme.media._480} {
+    gap: 5px;
+    & > div {
+      font-size: 12px;
+    }
+    & > button {
+      font-size: 12px;
+      padding: 2px 10px;
+    }
   }
 `

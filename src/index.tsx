cc0c45b19cd4da2fd7f26757717cbb29138c43ea
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:05:42 +0300

    add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,40 @@ import {ThemeProvider} from "styled-components";
 import {theme} from "./assets/styles/theme";
 import {Provider} from "react-redux";
 import {store} from './core/redux/store'
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
 import 'antd/dist/antd.min.css';
 import './assets/styles/index.css';
+
+i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        fallbackLng: "ru",
+        detection: {
+            order: ['localStorage', 'cookie', 'htmlTag', 'path', 'subdomain'],
+            caches: ['localStorage']
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/translation.json',
+        }
+    });
 
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-        <BrowserRouter>
-            <Provider store={store}>
+    <BrowserRouter>
+        <Provider store={store}>
             <ThemeProvider theme={theme}>
-            <App/>
+                <App/>
             </ThemeProvider>
-            </Provider>
-        </BrowserRouter>
+        </Provider>
+    </BrowserRouter>
 );
 

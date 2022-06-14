a9e81bd00051a93a503e8c888e95a6621f550601
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 10:59:14 +0300

    refactor

--- src/api/auth/authService.ts
+++ src/api/auth/authService.ts
@@ -2,15 +2,14 @@ import {IAuthFromServes, SignUpType} from "./authDto";
 import {post, get, patch} from "../baseRequest";
 
 
-export const signUpService = (data: SignUpType): Promise<IAuthFromServes> => {
-    return post('users', JSON.stringify(data))
-}
+export const signUpService = (data: SignUpType): Promise<IAuthFromServes> => post('users', JSON.stringify(data))
 
-export const searchEmailUsers = (email: string): Promise<IAuthFromServes[]> => {
-    return get(`users?email=${email}`)
-}
+export const searchEmailUsers = (email: string): Promise<IAuthFromServes[]> => get(`users?email=${email}`)
+
+export const getByIdUser = (id: string): Promise<IAuthFromServes[]> => get(`users?id=${id}`)
+
+export const updatePasswordUser = (id: string, password: string): Promise<IAuthFromServes> => patch(`users/${id}`, JSON.stringify({password}))
 
 
-export const updatePasswordUser = (id: string, password: string): Promise<IAuthFromServes> => {
-    return patch(`users/${id}`, JSON.stringify({password}))
-}
+
+

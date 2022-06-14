ada94e25df45b1074e5614723173b535703cb51e
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:01:21 +0300

    refactor

--- src/api/customers/customersService.ts
+++ src/api/customers/customersService.ts
@@ -2,18 +2,12 @@ import {SeekerType, EmployeeType, EmployeeFromServiceType, SeekerFromServiceType
 import {post, get, remove} from "../baseRequest";
 
 
-export const addSeeker = (data : SeekerType): Promise<SeekerFromServiceType[]> => {
-    return post('seeker', JSON.stringify(data))
-}
+export const addSeeker = (data : SeekerType): Promise<SeekerFromServiceType[]> => post('seeker', JSON.stringify(data))
 export const getAllSeeker = (): Promise<SeekerFromServiceType[]> => get('seeker')
-export const deleteIdSeeker = (id: number): Promise<SeekerFromServiceType[]> => remove(`seeker/${id}`)
+export const deleteIdSeeker = (id: number): Promise<void> => remove(`seeker/${id}`)
 
 
-
-export const addEmployee = (data : EmployeeType): Promise<EmployeeFromServiceType[]> => {
-    return post('employee', JSON.stringify(data))
-}
+export const addEmployee = (data : EmployeeType): Promise<EmployeeFromServiceType[]> => post('employee', JSON.stringify(data))
 export const getAllEmployee = (): Promise<EmployeeFromServiceType[]> => get('employee')
-
-export const deleteIdEmployee = (id: number): Promise<EmployeeFromServiceType[]> => remove(`employee/${id}`)
+export const deleteIdEmployee = (id: number): Promise<void> => remove(`employee/${id}`)
 

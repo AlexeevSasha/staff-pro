4f5a4e2e058eabae0406238a3feef1a5c7cef0f4
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:01:04 +0300

    adaptive

--- src/modules/customers/Customers.tsx
+++ src/modules/customers/Customers.tsx
@@ -2,18 +2,36 @@ import {FC} from "react";
 import {Tabs} from 'antd';
 import {Seeker} from "./components/Seeker";
 import {Employee} from "./components/Employee";
+import styled from "styled-components";
 
 const { TabPane } = Tabs;
 
 export const Customers: FC = () => {
   return (
-      <Tabs  type="card">
+      <TabsStyle  type="card">
         <TabPane tab="Seeker" key="1">
             <Seeker/>
         </TabPane>
         <TabPane tab="Employee" key="2">
          <Employee/>
         </TabPane>
-      </Tabs>
+      </TabsStyle>
   )
 }
+
+const TabsStyle = styled(Tabs)`
+  gap: 20px;
+
+  &.ant-tabs>.ant-tabs-nav, .ant-tabs>div>.ant-tabs-nav{
+    padding: 0 20px;
+    background: white;
+    margin: 0;
+  }
+  &.ant-tabs .ant-tabs-content-holder {
+    margin: 0 20px;
+  }
+
+  @media ${({theme}) => theme.media._768} {
+    gap: 10px;
+  }
+`

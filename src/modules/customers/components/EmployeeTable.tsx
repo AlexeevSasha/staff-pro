d68af1bde7f1cd1ac892b47c5b2ca25389885bc2
Alexeev Alexander <work.alexeevs@gmail.com>
2022-06-14 11:04:08 +0300

    adaptive

--- src/modules/customers/components/EmployeeTable.tsx
+++ src/modules/customers/components/EmployeeTable.tsx
@@ -1,18 +1,12 @@
 import {FC, useMemo} from "react";
 import {Popconfirm, Table} from "antd";
-import type { ColumnsType } from 'antd/lib/table';
+import type {ColumnsType} from 'antd/lib/table';
 import {EmployeeFromServiceType} from "../../../api/customers/customersDto";
-import {getAge} from "../../../utils/getAge";
-import {firstCharUppercase} from "../../../utils/firstCharUppercase";
+import {getAge} from "../../../core/utils/getAge";
+import {firstCharUppercase} from "../../../core/utils/firstCharUppercase";
 import {useAppDispatch} from "../../../core/redux/reduxType";
 import {deleteEmployeeThunk} from "../customersThunk";
-
-
-
-
-
-
-
+import {WrapperDisplayNone768} from "../../../common/components/Style";
 
 
 
@@ -22,22 +16,26 @@ interface IProps {
 
 export const EmployeeTable: FC<IProps> = ({employee}) => {
     const dispatch = useAppDispatch()
-    const handleDelete = (id : number) => {
+    const handleDelete = (id: number) => {
         dispatch(deleteEmployeeThunk(id))
     }
 
+
     const columns: ColumnsType<EmployeeFromServiceType> = useMemo(() => [
-        { title: 'Name', dataIndex: 'name', key: 'id',
+        {
+            title: 'Name', dataIndex: 'name', key: 'id',
             render: (_, {name, lastname}) => <div>{firstCharUppercase(name)}&nbsp;{firstCharUppercase(lastname)}</div>
         },
-        {title: 'Age', dataIndex: 'age', key: 'id',
-            render: (_, { year, month, day}) => <div>{getAge(`${year}/${month}/${day}`)}</div>,
-            sorter: (a, b) => getAge(`${a.year}/${a.month}/${a.day}`)  - getAge(`${b.year}/${b.month}/${b.day}`),
+        {
+            title: 'Age', dataIndex: 'age', key: 'id',
+            render: (_, {year, month, day}) => <div>{getAge(`${year}/${month}/${day}`)}</div>,
+            sorter: (a, b) => getAge(`${a.year}/${a.month}/${a.day}`) - getAge(`${b.year}/${b.month}/${b.day}`),
             sortDirections: ['ascend', 'descend', 'ascend'],
         },
-        {title: 'Position', dataIndex:'position', key: 'id'},
-        {title: 'Email', dataIndex:'email', key: 'id'},
-        {title: 'Sex', dataIndex: 'sex', key: 'id',
+        {title: 'Position', dataIndex: 'position', key: 'id'},
+        {title: 'Email', dataIndex: 'email', key: 'id'},
+        {
+            title: 'Sex', dataIndex: 'sex', key: 'id',
             filters: [
                 {
                     text: 'man',
@@ -53,7 +51,7 @@ export const EmployeeTable: FC<IProps> = ({employee}) => {
         {
             title: 'Delete',
             dataIndex: 'delete',
-            render: (_, {id}) =>(
+            render: (_, {id}) => (
                 <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(id)}>
                     <a>Delete</a>
                 </Popconfirm>
@@ -61,11 +59,14 @@ export const EmployeeTable: FC<IProps> = ({employee}) => {
         },
     ], [])
     return (
-        <Table
-            columns={columns}
-            dataSource={ employee}
-            rowKey="id"
-            pagination={{pageSize: 10}}
-        />
+            <WrapperDisplayNone768>
+                <Table
+
+                    columns={columns}
+                    dataSource={employee}
+                    rowKey="id"
+                    pagination={{pageSize: 10}}
+                />
+            </WrapperDisplayNone768>
     )
 }
